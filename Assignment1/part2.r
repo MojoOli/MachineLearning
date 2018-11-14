4b13153c28f8077a32df80a16dcaa0e4f62615a0
Robert Gstöttner <Robert@Macbook.local>
2018-11-14 21:51:48 +0100

    finish_bobs

--- Assignment1/part2.r
+++ Assignment1/part2.r
@@ -1,4 +1,8 @@
 library(ggplot2)
+library(lattice)
+library(caret)
+library(plyr)
+
 data(diamonds)
 
 # part 1
@@ -10,9 +14,32 @@ plot(diamonds$color)
 summary(diamonds$cut)
 summary(diamonds$clarity)
 
+# part 2
+qplot(diamonds$price)
+boxplot(diamonds$price)
+densityplot(diamonds$price, plot.points = F)
+
 # part 3
 summary(diamonds$price)
 sd(diamonds$price)
 mad(diamonds$price)
 
-#part 5
+# part 4
+plot(diamonds$price,diamonds$carat,pch = ".")
+
+# part 5
+
+# part 6
+boxplot(diamonds$price~diamonds$color)
+featurePlot(diamonds$price, diamonds$color,col=diamonds$color, plot = 'density', plot.points = F)
+?densityplot
+
+str(diamonds)
+numerics<-Filter(is.numeric, diamonds)
+# part 8
+aaply(.data = as.matrix(numerics), .margins = 2, .fun = mean)
+aaply(.data = as.matrix(numerics), .margins = 2, .fun = median)
+aaply(.data = as.matrix(numerics), .margins = 2, .fun = sd)
+aaply(.data = as.matrix(numerics), .margins = 2, .fun = function(col){
+  mad(col,constant = 1)
+})
