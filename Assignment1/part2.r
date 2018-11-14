2131a5e85619812b2e4d9a5c2d231245f1b3713d
x_oli <x_oliver_x@hotmail.de>
2018-11-14 21:44:27 +0100

    finished task 5 and task 7

--- Assignment1/part2.r
+++ Assignment1/part2.r
@@ -1,3 +1,4 @@
+setwd("D:/FHHagenberg/Master/S3/ML/Assignments/Assignment1")
 library(ggplot2)
 data(diamonds)
 
@@ -5,10 +6,9 @@ data(diamonds)
 ?diamonds
 
 str(diamonds)
-summary(diamonds$color)
-plot(diamonds$color)
-summary(diamonds$cut)
-summary(diamonds$clarity)
+max(summary(diamonds$color)) / min(summary(diamonds$color))
+max(summary(diamonds$cut)) / min(summary(diamonds$cut))
+max(summary(diamonds$clarity)) / min(summary(diamonds$clarity))
 
 # part 3
 summary(diamonds$price)
@@ -16,3 +16,16 @@ sd(diamonds$price)
 mad(diamonds$price)
 
 #part 5
+pairs(diamonds[,7:10], upper.panel = NULL, pch='.')
+
+#part 7
+dim(diamonds[diamonds$price > 9500.0,])
+dim(diamonds[diamonds$price > 9500.0 & diamonds$color == "D",])
+
+diamonds_filtered1 = diamonds[diamonds$color == "D" & diamonds$cut == "Fair",]
+summary(diamonds_filtered1$price)
+summary(diamonds_filtered1$carat)
+
+diamonds_filtered2 = diamonds[diamonds$color == "J" & diamonds$cut == "Ideal",]
+summary(diamonds_filtered2$price)
+summary(diamonds_filtered2$carat)
