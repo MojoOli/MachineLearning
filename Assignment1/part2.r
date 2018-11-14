5df3d4dbdb820f9213e132b96cc176ab41ae3ea0
x_oli <x_oliver_x@hotmail.de>
2018-11-14 19:55:29 +0100

    finished 1. 3.

--- Assignment1/part2.r
+++ Assignment1/part2.r
@@ -1,2 +1,18 @@
 library(ggplot2)
 data(diamonds)
+
+# part 1
+?diamonds
+
+str(diamonds)
+summary(diamonds$color)
+plot(diamonds$color)
+summary(diamonds$cut)
+summary(diamonds$clarity)
+
+# part 3
+summary(diamonds$price)
+sd(diamonds$price)
+mad(diamonds$price)
+
+#part 5
