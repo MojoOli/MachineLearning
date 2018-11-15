1ff238a25954fef154276a9d93f9ff166a63689b
AnastasiiaMishchenko <mischenkonastya@gmail.com>
2018-11-15 11:01:08 +0100

    part3

--- Assignment1/part3.r
+++ Assignment1/part3.r
@@ -1,8 +1,15 @@
-#task 1
+getwd()
+setwd('ENI/MC520/Assignment\_1')
 
-#task 2
+csv_file <- read.csv('segmentationData.csv')
+str(csv_file)
+is.factor(csv_file$Class)
 
+# task 1
+summary(csv_file$Class)
 
-#task 3
+# task 2
+pairs(csv_file, pch = '.', upper.panel = NULL, col=csv_file$Class)
+plot(csv_file[,c(6,8)], pch = '.', xlab = 'AvgIntenCh3', ylab = 'ConvexHullAreaRationCh1', col=csv_file$Class)
+plot(csv_file[,c(6,9)], pch = '.', xlab = 'AvgIntenCh3', ylab = 'ConvexHullPerimRationCh1', col=csv_file$Class)
 
-#...
