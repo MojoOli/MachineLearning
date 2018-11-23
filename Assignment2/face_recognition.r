bdec51d1d6064ecd071e31b5a2ef3c93b5ad9e80
x_oli <x_oliver_x@hotmail.de>
2018-11-23 22:58:49 +0100

    added edge detection and sliding windows approach

--- Assignment2/face_recognition.r
+++ Assignment2/face_recognition.r
@@ -28,7 +28,7 @@ library(png)
 library(plyr)
 library(caret) # caret ML framework
 library(doMC) # parallelization
-registerDoMC(3) # register 3 cores (more cores require more RAM)
+registerDoMC(7) # register 7 cores (more cores require more RAM)
 ###
 
 ### Import data
@@ -41,6 +41,42 @@ imgData <- ldply(dir(path = 'dsr-preprocessed-1000x1333_faces_haar_gray_resized1
                        as.numeric(strsplit(f, '_')[[1]][8]),
                        as.numeric(readPNG(f))))
                    });
+
+####### Edge Detection
+
+# convert data to matrix to be able to use plyr without variable grouping 
+# and to calculate the imgradient for every picture
+
+gMagData <- alply(as.matrix(imgData[,2:2501]), .margins=1, .fun=function(x){
+  cImgData <- as.cimg(matrix(data = x, nrow = 50, ncol = 50))
+  imgradient(cImgData,"xy") %>% enorm
+})
+
+# show difference in images
+normalImage <- as.cimg(matrix(data = unlist(imgData[1,2:2501]), nrow = 50, ncol = 50))
+sidePlot <- as.imlist(list(grayscale=normalImage, "gradient magnitude" =gMagData[[1]]))
+plot(sidePlot, layout="row")
+
+# threshold(gMagData[[1]],"60%") %>% plot(main="Determinant: 40% highest values")
+
+# convert to data frame
+# threshold where only the 40% highest values will be taken into account
+gMagDataFrame <- ldply(gMagData, .fun=function(x){
+  base<-x[,,1,1]
+  as.vector(base)
+  # as.numeric(threshold(base,"60%"))
+})
+gMagDataFrame<-Filter(is.numeric, gMagDataFrame)
+
+#######
+
+### Sliding window approach
+library(zoo)
+swImgDataTransposed <- rollapply(data = t(imgData[,2:2501]), width = 5, by=5, FUN = median)
+swImgData = as.data.frame(t(swImgDataTransposed))
+dim(swImgData)
+###
+
 # Convert the person number into a "normalized" string by adding P and ensuring it is always (at least) 3 characters long
 # Then convert it to a factor (a class label)
 imgData[,1] <- as.factor(sprintf("P%02d", imgData[,1]))
@@ -49,10 +85,9 @@ summary(imgData$`1`)
 ### 
 
 ### Draw correlation plots for the first couple of features
-corrplot(cor(imgData[,2:450]))
 corrplot(cor(imgData[,2:50]))
-corrplot(cor(imgData[,1001:1200]))
-corrplot(cor(imgData[,1201:1500]))
+corrplot(cor(imgData[,1001:1101]))
+corrplot(cor(t(imgData[1:60,1201:1301])))
 ###
 
 ### Training + Setup
@@ -65,13 +100,14 @@ trControl <- trainControl(method = 'repeatedcv',
                           returnResamp = 'final', 
                           allowParallel = T)
 
+
 ## LDA Model 
 # Preprocessing: centering, scaling and PCA 
 # Partitioning: 10CV Repeated 20 times
 names(getModelInfo('lda')) # linear discriminant analysis
 getModelInfo('lda')[[1]]$parameters # this model does not have any hyperparameters
-models$modelLda <- train(x = imgData[,2:2500], 
-                         y = imgData[,1], 
+models$modelLda <- train(x = gMagDataFrame,
+                         y = imgData[,1],
                          preProcess = c('center', 'scale', 'pca'), 
                          method = 'lda', 
                          tuneGrid = NULL, 
