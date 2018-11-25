ac1ae1c5ea4ec8280aedb7d53cfa628aab2d8c41
AnastasiiaMishchenko <mischenkonastya@gmail.com>
2018-11-25 18:39:22 +0100

    model training and testing

--- Assignment2/face_recognition.r
+++ Assignment2/face_recognition.r
@@ -23,7 +23,11 @@
 ###########################################################################################
 
 ### Libraries and workspace setup
+<<<<<<< HEAD
+library(imager)
+=======
 install.packages("doMC")
+>>>>>>> origin/master
 library(corrplot)
 library(png)
 library(plyr)
@@ -75,17 +79,37 @@ gMagDataFrame<-Filter(is.numeric, gMagDataFrame)
 
 ### Sliding window approach
 library(zoo)
-swImgDataTransposed <- rollapply(data = t(imgData[,2:2501]), width = 5, by=5, FUN = median)
-swImgData = as.data.frame(t(swImgDataTransposed))
-dim(swImgData)
-###
+windowSize<-4
+swImgData <- alply(as.matrix(imgData[,2:2501]), .margins=1, .fun=function(x){
+  imgData <- matrix(data = x, nrow = 50, ncol = 50)
+  imgData <- rollapply(data = imgData, width = windowSize, by = windowSize, FUN = IQR)
+  imgData <- as.cimg(t(rollapply(data = t(imgData), width = windowSize, by = windowSize, FUN = IQR)))
+})
 
-# Convert the person number into a "normalized" string by adding P and ensuring it is always (at least) 3 characters long
-# Then convert it to a factor (a class label)
-imgData[,1] <- as.factor(sprintf("P%02d", imgData[,1]))
-levels(imgData$`1`)
-summary(imgData$`1`)
-### 
+# show difference in images
+sidePlot <- as.imlist(list(grayscale=normalImage, "sliding window image" = swImgData[[1]]))
+plot(sidePlot, layout="row", xlab="pixel", ylab="pixel")
+
+# convert to data frame
+swImgDataFrame <- ldply(swImgData, .fun=function(x){
+  base<-x[,,1,1]
+  as.vector(base)
+})
+# get rid of levels
+swImgDataFrame<-Filter(is.numeric, swImgDataFrame)
+
+# data partitioning, group by person
+indexes_train = createDataPartition(imgData$`1`, p=0.75, list=F)
+indexes_test = (1:nrow(imgData))[-indexes_train]
+
+rawDataTraining <- imgData[indexes_train,]
+rawDataTesting <- imgData[indexes_test,]
+
+gMagDataTraining <- gMagDataFrame[indexes_train,]
+gMagDataTesting <- gMagDataFrame[indexes_test,]
+
+swImgDataTrainig <- swImgDataFrame[indexes_train,]
+swImgDataTesting <- swImgDataFrame[indexes_test,]
 
 ### Draw correlation plots for the first couple of features
 corrplot(cor(imgData[,2:50]))
@@ -107,11 +131,18 @@ trControl <- trainControl(method = 'repeatedcv',
 ## LDA Model 
 # Preprocessing: centering, scaling and PCA 
 # Partitioning: 10CV Repeated 20 times
+# Raw data
+# models$modelLda <- readRDS('modelRawDataLda.rds')
 names(getModelInfo('lda')) # linear discriminant analysis
 getModelInfo('lda')[[1]]$parameters # this model does not have any hyperparameters
+<<<<<<< HEAD
+models$modelLda <- train(x = rawDataTraining[,-1], # exclude the person id 
+                         y = rawDataTraining[,1],
+=======
 ?train
 models$modelLda <- train(x = gMagDataFrame,
                          y = imgData[,1],
+>>>>>>> origin/master
                          preProcess = c('center', 'scale', 'pca'), 
                          method = 'lda', 
                          tuneGrid = NULL, 
@@ -121,6 +152,246 @@ models$modelLda
 cvConfMatrix <- confusionMatrix(models$modelLda)
 cvConfMatrix
 levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
+saveRDS(models$modelLda, file='modelRawDataLda.rds')
+
+# gMagDataFrame
+# models$modelLdaGMag <- readRDS('modelLdaGMag.rds')
+models$modelLdaGMag <- train(x = gMagDataTraining,
+                         y = rawDataTraining[,1],
+                         preProcess = c('center', 'scale', 'pca'), 
+                         method = 'lda', 
+                         tuneGrid = NULL, 
+                         metric = 'Kappa', 
+                         trControl = trControl)
+models$modelLdaGMag
+cvConfMatrix <- confusionMatrix(models$modelLdaGMag)
+cvConfMatrix
+levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
+saveRDS(models$modelLdaGMag, file='modelLdaGMag.rds')
+
+# sw approach
+# models$modelLdaSW <- readRDS('modelLdaSW.rds')
+models$modelLdaSW <- train(x = swImgDataTrainig,
+                         y = rawDataTraining[,1],
+                         preProcess = c('center', 'scale', 'pca'), 
+                         method = 'lda', 
+                         tuneGrid = NULL, 
+                         metric = 'Kappa', 
+                         trControl = trControl)
+models$modelLdaSW
+cvConfMatrix <- confusionMatrix(models$modelLdaSW)
+cvConfMatrix
+levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
+saveRDS(models$modelLdaSW, file='modelLdaSW.rds')
 ##
 
+## KNN model
+# Raw data
+# models$modelKnnRawData <- readRDS('modelKnnRawData.rds')
+names(getModelInfo('knn'))
+getModelInfo('knn')[[2]]$parameters  # this model has hyperparameters k (number of neighbors)
+models$modelKnnRawData <- train(x = rawDataTraining[,-1], # exclude the person id 
+                  y = rawDataTraining[,1],
+                  preProcess = c('center', 'scale', 'pca'),  
+                  method = 'knn',
+                  tuneGrid = expand.grid(k=1:10), 
+                  metric = 'Kappa', 
+                  trControl = trControl)
+models$modelKnnRawData
+cvConfMatrix <- confusionMatrix(models$modelKnnRawData)
+cvConfMatrix
+levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
+saveRDS(models$modelKnnRawData, file='modelKnnRawData.rds')
+
+# gMagDataFrame
+# models$modelKnnGMagwData <- readRDS('modelKnnGMagwData.rds')
+models$modelKnnGMagwData <- train(x = gMagDataTraining, 
+                                y = rawDataTraining[,1],
+                                preProcess = c('center', 'scale', 'pca'),  
+                                method = 'knn',
+                                tuneGrid = expand.grid(k=1:10), 
+                                metric = 'Kappa', 
+                                trControl = trControl)
+models$modelKnnGMagwData
+cvConfMatrix <- confusionMatrix(models$modelKnnGMagwData)
+cvConfMatrix
+levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
+saveRDS(models$modelKnnGMagwData, file='modelKnnGMagwData.rds')
+
+# sw approach
+# models$modelKnnSW <- readRDS('modelKnnSW.rds')
+models$modelKnnSW <- train(x = swImgDataTrainig, 
+                                  y = rawDataTraining[,1],
+                                  preProcess = c('center', 'scale', 'pca'),  
+                                  method = 'knn',
+                                  tuneGrid = expand.grid(k=1:10), 
+                                  metric = 'Kappa', 
+                                  trControl = trControl)
+models$modelKnnSW
+cvConfMatrix <- confusionMatrix(models$modelKnnSW)
+cvConfMatrix
+levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
+saveRDS(models$modelKnnSW, file='modelKnnSW.rds')
+
+## random forest
+# Raw data
+# models$modelRandomForestRawData <- readRDS('modelRandomForestRawData.rds')
+names(getModelInfo('rf')) # random forest
+getModelInfo('rf')[[2]]$parameters
+models$modelRandomForestRawData <- train( x = rawDataTraining[,-1], 
+                                   y = rawDataTraining[,1], 
+                                   preProcess = c('center', 'scale', 'pca'),   
+                                   method = 'rf',
+                                   tuneGrid = expand.grid(mtry=1:4),
+                                   metric = 'Kappa', 
+                                   trControl = trControl)
+models$modelRandomForestRawData
+plot(models$modelRandomForestRawData)
+models$modelRandomForestRawData$finalModel
+plot(models$modelRandomForestRawData$finalModel)
+cvConfMatrix <- confusionMatrix(models$modelRandomForestRawData)
+cvConfMatrix
+levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
+saveRDS(models$modelRandomForestRawData, file='modelRandomForestRawData.rds')
+
+# gMadData
+# models$modelRandomForestGMagData <- readRDS('modelRandomForestGMagData.rds')
+models$modelRandomForestGMagData <- train( x = gMagDataTraining, 
+                                          y = rawDataTraining[,1], 
+                                          preProcess = c('center', 'scale', 'pca'),   
+                                          method = 'rf',
+                                          tuneGrid = expand.grid(mtry=1:4),
+                                          metric = 'Kappa', 
+                                          trControl = trControl)
+models$modelRandomForestGMagData
+plot(models$modelRandomForestGMagData)
+models$modelRandomForestGMagData$finalModel
+plot(models$modelRandomForestGMagData$finalModel)
+cvConfMatrix <- confusionMatrix(models$modelRandomForestGMagData)
+cvConfMatrix
+levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
+saveRDS(models$modelRandomForestGMagData, file='modelRandomForestGMagData.rds')
+
+# sw approach
+# models$modelRandomForestSW  <- readRDS('modelRandomForestSW.rds')
+# models$modelRandomForestSWMean  <- readRDS('modelRandomForestSWMean.rds')
+# models$modelRandomForestSWMad  <- readRDS('modelRandomForestSWMad.rds')
+# models$modelRandomForestSWIQR  <- readRDS('mmodelRandomForestSWIQR.rds')
+models$modelRandomForestSWIQR <- train( x = swImgDataTrainig, 
+                                        y = rawDataTraining[,1], 
+                                        preProcess = c('center', 'scale', 'pca'),   
+                                        method = 'rf',
+                                        tuneGrid = expand.grid(mtry=1:4),
+                                        metric = 'Kappa', 
+                                        trControl = trControl)
+models$modelRandomForestSWIQR
+saveRDS(models$modelRandomForestSWIQR, file='modelRandomForestSWIQR.rds')
+
+models$modelRandomForestSWMad <- train( x = swImgDataTrainig, 
+                                         y = rawDataTraining[,1], 
+                                         preProcess = c('center', 'scale', 'pca'),   
+                                         method = 'rf',
+                                         tuneGrid = expand.grid(mtry=1:4),
+                                         metric = 'Kappa', 
+                                         trControl = trControl)
+models$modelRandomForestSWMad
+saveRDS(models$modelRandomForestSWMad, file='modelRandomForestSWMad.rds')
+
+models$modelRandomForestSWMean <- train( x = swImgDataTrainig, 
+                                     y = rawDataTraining[,1], 
+                                     preProcess = c('center', 'scale', 'pca'),   
+                                     method = 'rf',
+                                     tuneGrid = expand.grid(mtry=1:4),
+                                     metric = 'Kappa', 
+                                     trControl = trControl)
+models$modelRandomForestSWMean
+saveRDS(models$modelRandomForestSWMean, file='modelRandomForestSWMean.rds')
+
+models$modelRandomForestSW <- train( x = swImgDataTrainig, 
+                                           y = rawDataTraining[,1], 
+                                           preProcess = c('center', 'scale', 'pca'),   
+                                           method = 'rf',
+                                           tuneGrid = expand.grid(mtry=1:4),
+                                           metric = 'Kappa', 
+                                           trControl = trControl)
+models$modelRandomForestSW
+plot(models$modelRandomForestSW)
+models$modelRandomForestSW$finalModel
+cvConfMatrix <- confusionMatrix(models$modelRandomForestSW)
+cvConfMatrix
+levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
+saveRDS(models$modelRandomForestSW, file='modelRandomForestSW.rds')
+
+results <- resamples(list(RFMEAD=models$modelRandomForestSW, RFMEAN=models$modelRandomForestSWMean, RFIQR=models$modelRandomForestSWIQR, RFMAD=models$modelRandomForestSWMad))
+summary(results)
+scales <- list(x=list(relation="free"), y=list(relation="free"))
+bwplot(results, scales=scales)
+
+
+## Naive Bayes
+# Raw Data
+# models$modelNaiveBayesRawData <- readRDS('modelNaiveBayesRawData.rds')
+names(getModelInfo('nb')) # naive bayes 
+getModelInfo('nb')[[1]]$parameters 
+models$modelNaiveBayesRawData <- train( x = rawDataTraining[,-1], 
+                                 y = rawDataTraining[,1], 
+                                 preProcess = c('center', 'scale', 'pca'),   
+                                 method = 'nb', 
+                                 tuneGrid = NULL,
+                                 metric = 'Kappa', 
+                                 trControl = trControl)
+models$modelNaiveBayesRawData
+plot(models$modelNaiveBayesRawData)
+str(models$modelNaiveBayesRawData$finalModel) 
+cvConfMatrix <- confusionMatrix(models$modelNaiveBayesRawData)
+cvConfMatrix
+levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
+saveRDS(models$modelNaiveBayesRawData, file='modelNaiveBayesRawData.rds')
+
+# gMag Data
+# models$modelNaiveBayesGMagData <- readRDS('modelNaiveBayesGMagData.rds')
+models$modelNaiveBayesGMagData <- train( x = gMagDataTraining, 
+                                         y = rawDataTraining[,1], 
+                                        preProcess = c('center', 'scale', 'pca'),   
+                                        method = 'nb', 
+                                        tuneGrid = NULL,
+                                        metric = 'Kappa', 
+                                        trControl = trControl)
+models$modelNaiveBayesGMagData
+plot(models$modelNaiveBayesGMagData)
+str(models$modelNaiveBayesGMagData$finalModel) 
+cvConfMatrix <- confusionMatrix(models$modelNaiveBayesGMagData)
+cvConfMatrix
+levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
+saveRDS(models$modelNaiveBayesGMagData, file='modelNaiveBayesGMagData.rds')
+
+
+# SW approach
+# models$modelNaiveBayesSW <- readRDS('modelNaiveBayesSW.rds')
+models$modelNaiveBayesSW <- train( x = swImgDataTrainig, 
+                                   y = rawDataTraining[,1],  
+                                        preProcess = c('center', 'scale', 'pca'),   
+                                        method = 'nb', 
+                                        tuneGrid = NULL,
+                                        metric = 'Kappa', 
+                                        trControl = trControl)
+models$modelNaiveBayesSW
+plot(models$modelNaiveBayesSW)
+str(models$modelNaiveBayesSW$finalModel) 
+cvConfMatrix <- confusionMatrix(models$modelNaiveBayesSW)
+cvConfMatrix
+levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
+saveRDS(models$modelNaiveBayesSW, file='modelNaiveBayesSW.rds')
+
+results <- resamples(list(LDARawData=models$modelLda, LDAGMagData=models$modelLdaGMag, LDASW=models$modelLdaSW, KNNRawData=models$modelKnnRawData, KNNGMadData=models$modelKnnGMagwData, KNNSW=models$modelKnnSW, RFRawData=models$modelRandomForestRawData, RFGMagData=models$modelRandomForestGMagData, RFSW=models$modelRandomForestSW, NBRawData=models$modelNaiveBayesRawData, NBGMagData=models$modelNaiveBayesGMagData, NBSW=models$modelNaiveBayesSW))
+summary(results)
+scales <- list(x=list(relation="free"), y=list(relation="free"))
+bwplot(results, scales=scales)
+
+# Testing with RF
+predictedFinal <- predict(models$modelRandomForestRawData, newdata=rawDataTesting[,-1])
+conf <- confusionMatrix(data = predictedFinal, reference = rawDataTesting[,1])
+levelplot(sweep(x = conf$table, STATS = colSums(conf$table), MARGIN = 2, FUN = `/`), 
+          col.regions=gray(100:0/100))
+plot(models$modelRandomForestRawData)
 ###
