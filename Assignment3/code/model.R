db49eddf10e884603557ceb01df12dbad1aa1c70
Appyx <robert.gstoettner@icloud.com>
2018-12-08 22:20:34 +0100

    images

--- Assignment3/code/model.R
+++ Assignment3/code/model.R
@@ -18,85 +18,102 @@ phase <- getFeaturePhase(generateMagnitude(raw_x, raw_y, raw_z))
 sc <- getSpectralCentroid(power)
 
 # spectrum band energy
-band_energy <- getBandEnergy(power, 40)
+band_energy <- getBandEnergy(power, 20)
 
 
 #sliding window
 sw_mad <- slidingWindow(data, mad, 20)
 sw_IQR <- slidingWindow(data, IQR, 20)
 sw_median <- slidingWindow(data, median, 20)
+sw_max <-slidingWindow(data,max,20)
+sw_min <-slidingWindow(data,min,20)
 
 
-df<-data.frame(length_x,sw_mad,sw_IQR,sw_median,sc,band_energy)
+df<-data.frame(length_x,sw_mad,sw_IQR,sw_median,sw_max,sw_min,sc,band_energy)
 
 
-#models: (svm,lda,lda2,knn,rf)
+#models: (svm-linear,lda,knn,rf)
 models <- list()
 registerDoMC(10) # register 3 cores (more cores require more RAM)
 
 trControl <- trainControl(method = 'repeatedcv', 
-                          number = 5, 
-                          repeats = 10, 
+                          number = 10, 
+                          repeats = 5, 
                           returnData = F, 
                           classProbs = T, 
                           returnResamp = 'final', 
                           allowParallel = T)
 
 
+###LDA###
+names(getModelInfo('lda')) # linear discriminant analysis
+getModelInfo('lda')[[1]]$parameters # this model does not have any hyperparameters
+models$lda <- train(x = df, # exclude the person id 
+                         y = general_data[,1],
+                         preProcess = c('center', 'scale', 'pca'), 
+                         method = 'lda', 
+                         tuneGrid = NULL, 
+                         metric = 'Kappa', 
+                         trControl = trControl)
+models$lda
+cvConfMatrix <- confusionMatrix(models$lda)
+cvConfMatrix
+levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
+#saveRDS(models$modelLda, file='lda.rds')
+
 ###KNN###
 
 names(getModelInfo('knn'))
-getModelInfo('knn')[[2]]$parameters  # this model has hyperparameters k (number of neighbors)
-models$modelKnnRawData <- train(x = df, # exclude the person id 
+getModelInfo('knn')[[2]]$parameters  #  k numeric #Neighbors
+models$knn <- train(x = df, # exclude the person id 
                                 y = general_data[,1],
                                 preProcess = c('center', 'scale', 'pca'),  
                                 method = 'knn',
                                 tuneGrid = expand.grid(k=1:50), 
                                 metric = 'Kappa', 
                                 trControl = trControl)
-models$modelKnnRawData
-cvConfMatrix <- confusionMatrix(models$modelKnnRawData)
+models$knn
+cvConfMatrix <- confusionMatrix(models$knn)
 cvConfMatrix
+plot(models$knn)
 levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
-#saveRDS(models$modelKnnRawData, file='modelKnnRawData.rds')
+#saveRDS(models$modelKnnRawData, file='knn.rds')
 
 
 ###Random-Forest###
 names(getModelInfo('rf')) # random forest
-getModelInfo('rf')[[2]]$parameters
-models$modelRandomForestRawData <- train( x = df, 
+getModelInfo('rf')[[2]]$parameters #mtry numeric #Randomly Selected Predictors
+models$rf <- train( x = df, 
                                           y = general_data[,1], 
                                           preProcess = c('center', 'scale', 'pca'),   
                                           method = 'rf',
-                                          tuneGrid = expand.grid(mtry=1:4),
+                                          tuneGrid = expand.grid(mtry=1:20),
                                           metric = 'Kappa', 
                                           trControl = trControl)
-models$modelRandomForestRawData
-plot(models$modelRandomForestRawData)
-models$modelRandomForestRawData$finalModel
-plot(models$modelRandomForestRawData$finalModel)
-cvConfMatrix <- confusionMatrix(models$modelRandomForestRawData)
+models$rf
+plot(models$rf)
+models$rf$finalModel
+plot(models$rf$finalModel)
+cvConfMatrix <- confusionMatrix(models$rf)
 cvConfMatrix
 levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
-#saveRDS(models$modelRandomForestRawData, file='modelRandomForestRawData.rds')
+#saveRDS(models$modelRandomForestRawData, file='rf.rds')
 
 
 
 ###SVM-Radial###
-names(getModelInfo('svm')) # linear discriminant analysis
-getModelInfo('svmLinear')[[1]]$parameters # this model does not have any hyperparameters
-models$modelLda <- train(x = df, # exclude the person id 
+names(getModelInfo('svmLinear')) # linear discriminant analysis
+getModelInfo('svmLinear')[[1]]$parameters # tau numeric Regularization Parameter
+models$svm <- train(x = df, # exclude the person id 
                          y = general_data[,1],
                          preProcess = c('center', 'scale', 'pca'), 
-                         method = 'svmRadial', 
-                         tuneGrid = expand.grid(C=3**(-5:5), sigma=3**(-5:5)), 
+                         method = 'svmLinear', 
+                         tuneGrid = expand.grid(C=3**(-3:3)), 
                          metric = 'Kappa', 
                          trControl = trControl)
-models$modelLda
-cvConfMatrix <- confusionMatrix(models$modelLda)
+models$svm
+cvConfMatrix <- confusionMatrix(models$svm)
 cvConfMatrix
 levelplot(sweep(x = cvConfMatrix$table, STATS = colSums(cvConfMatrix$table), MARGIN = 2, FUN = '/'), col.regions=gray(100:0/100))
-#saveRDS(models$modelLda, file='modelRawDataLda.rds')
+#saveRDS(models$modelLda, file='svm.rds')
 
-
-###SVM-Linear###
